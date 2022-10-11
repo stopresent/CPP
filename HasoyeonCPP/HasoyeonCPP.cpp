785bb52a4ea2e969c52d9a5f81a4439924e661e6
stopresent <mamy4938@gmail.com>
2022-10-11 15:17:10 +0900

    New Delete

--- HasoyeonCPP/HasoyeonCPP.cpp
+++ HasoyeonCPP/HasoyeonCPP.cpp
@@ -1,17 +1,44 @@
+/* new 와 delete 의 사용 */
 #include <iostream>
 using namespace std;
-#include "Game.h"
 
-Game game;
-
-int main()
+int CreateNewArray()
 {
-	// Text RPG
+	int arr_size;
 
-	while (true)
-	{
-		game.Init();
-	}
+	std::cout << "array size : ";
+	std::cin >> arr_size;
+
+	int* list = new int[arr_size];
+
+	for (int i = 0; i < arr_size; i++)
+		std::cin >> list[i];
+
+	for (int i = 0; i < arr_size; i++)
+		std::cout << i << "th element of list : " << list[i] << std::endl;
+	
+	delete[] list;
+	return 0;
+}
+
+int CreateNew()
+{
+	int* p = new int;
+	*p = 10;
+
+	std::cout << *p << std::endl;
+
+	delete p;
 
 	return 0;
 }
+
+int main()
+{
+	CreateNew();
+	CreateNewArray();
+
+	return 0;
+}
+
+// new 는 heap영역에 생김
