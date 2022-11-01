f453f9a9e9ebdaa360ca6a0212375cae93bcf791
stopresent <mamy4938@gmail.com>
2022-11-01 16:08:35 +0900

    LIS (Longest Increasing Sequence) 문제

--- HasoyeonCPP/HasoyeonCPP.cpp
+++ HasoyeonCPP/HasoyeonCPP.cpp
@@ -2,276 +2,41 @@
 using namespace std;
 #include <list>
 #include<utility>
+#include <vector>
 
 /*
+LIS (Longest Increasing Sequence)
 
-풍선 터뜨리기
+동적 계획법 (DP)
 
-문제
-1번부터 N번까지 N개의 풍선이 원형으로 놓여 있고. i번 풍선의 오른쪽에는 i+1번 풍선이 있고, 왼쪽에는 i-1번 풍선이 있다.
-단, 1번 풍선의 왼쪽에 N번 풍선이 있고, N번 풍선의 오른쪽에 1번 풍선이 있다.
-각 풍선 안에는 종이가 하나 들어있고, 종이에는 -N보다 크거나 같고, N보다 작거나 같은 정수가 하나 적혀있다. 이 풍선들을 다음과 같은 규칙으로 터뜨린다.
+Seq : 1 9 2 5 7
 
-우선, 제일 처음에는 1번 풍선을 터뜨린다. 다음에는 풍선 안에 있는 종이를 꺼내어 그 종이에 적혀있는 값만큼 이동하여 다음 풍선을 터뜨린다.
-양수가 적혀 있을 경우에는 오른쪽으로, 음수가 적혀 있을 때는 왼쪽으로 이동한다. 이동할 때에는 이미 터진 풍선은 빼고 이동한다.
+부분 수열 : 일부(0개 이상) 숫자를 지우고 남은 수열
 
-예를 들어 다섯 개의 풍선 안에 차례로 3, 2, 1, -3, -1이 적혀 있었다고 하자.
-이 경우 3이 적혀 있는 1번 풍선, -3이 적혀 있는 4번 풍선, -1이 적혀 있는 5번 풍선, 1이 적혀 있는 3번 풍선, 2가 적혀 있는 2번 풍선의 순서대로 터지게 된다.
+ex) 1 2 5
 
-입력
-첫째 줄에 자연수 N(1 ≤ N ≤ 1,000)이 주어진다. 다음 줄에는 차례로 각 풍선 안의 종이에 적혀 있는 수가 주어진다. 종이에 0은 적혀있지 않다.
+ex) 1 9 5 7
 
-출력
-첫째 줄에 터진 풍선의 번호를 차례로 나열한다.
+순 증가 부분 수열
 
-예제 입력 1
-5
-3 2 1 -3 -1
+ex) 1 2 5
 
-예제 출력 1
-1 4 5 3 2
+LIS : 제일 긴 [순 증가 부분 수열]의 길이
+
+ex) 1 2 5 7 = 길이 4
 
 */
 
-template<typename T>
-class Node
+vector<int> seq;
+
+int LIS()
 {
-public:
-	Node() : _prev(nullptr), _next(nullptr), _data(T())
-	{
 
-	}
-
-	Node(const T& value) : _prev(nullptr), _next(nullptr), _data(value)
-	{
-
-	}
-
-public:
-	Node*	_prev;
-	Node*	_next;
-	T		_data;
-	T		_index;
-};
-
-template<typename T>
-class Iterator
-{
-public:
-	Iterator() : _node(nullptr)
-	{
-
-	}
-
-	Iterator(Node<T>* node) : _node(node)
-	{
-
-	}
-
-	// it++
-	Iterator operator++(int)
-	{
-		Iterator<T> temp = *this;
-		_node = _node->_next;
-		return temp;
-	}
-
-	// it--
-	Iterator operator--(int)
-	{
-		Iterator<T> temp = *this;
-		_node = _node->_prev;
-		return temp;
-	}
-
-	// *it
-	T& operator*()
-	{
-		return _node->_data;
-	}
-
-	T& operator&()
-	{
-		return _node->_index;
-	}
-
-	bool operator==(const Iterator& other)
-	{
-		return _node == other._node;
-	}
-
-	bool operator!=(const Iterator& other)
-	{
-		return _node != other._node;
-	}
-
-public:
-	Node<T>* _node;
-};
-
-template<typename T>
-class List
-{
-public:
-	List() : _size(0)
-	{
-		// [head] <-> ... <-> [tail]
-		_head = new Node<T>();
-		_tail = new Node<T>();
-		_head->_next = _tail;
-		_tail->_prev = _head;
-	}
-
-	~List()
-	{
-		while (_size > 0)
-			pop_back();
-
-		delete _head;
-		delete _tail;
-	}
-
-	void push_back(const T& value, const T& indexValue)
-	{
-		AddNode(_tail, value, indexValue);
-	}
-
-	void pop_back()
-	{
-		RemoveNode(_tail->_prev);
-	}
-
-private:
-
-	// [head] <-> [1] <-> [prevNode] <-> [before] <-> [tail]
-	// [head] <-> [1] <-> [prevNode] <-> [newNode] <-> [before] <-> [tail]
-	Node<T>* AddNode(Node<T>* before, const T& value, const T& indexValue)
-	{
-		Node<T>* newNode = new Node<T>(value);
-		newNode->_index = indexValue;
-		Node<T>* prevNode = before->_prev;
-
-		prevNode->_next = newNode;
-		newNode->_prev = prevNode;
-
-		newNode->_next = before;
-		before->_prev = newNode;
-
-		_size++;
-
-		return newNode;
-	}
-
-	// [head] <-> [prevNode] <-> [node] <-> [nextNode] <-> [tail]
-	// [head] <-> [prevNode] <-> [nextNode] <-> [tail]
-	Node<T>* RemoveNode(Node<T>* node)
-	{
-		Node<T>* prevNode = node->_prev;
-		Node<T>* nextNode = node->_next;
-
-		prevNode->_next = nextNode;
-		nextNode->_prev = prevNode;
-
-		delete node;
-
-		_size--;
-
-		return nextNode;
-	}
-
-public:
-
-	int size() { return _size; }
-
-	using iterator = Iterator<T>;
-
-	iterator begin() { return iterator(_head->_next); }
-	iterator end() { return iterator(_tail); }
-
-	// it '앞에' 추가
-	iterator insert(iterator it, const T& value, const T& indexValue)
-	{
-		Node<T>* node = AddNode(it._node, value, indexValue);
-		return iterator(node);
-	}
-
-	iterator erase(iterator it)
-	{
-		Node<T>* node = RemoveNode(it._node);
-		return iterator(node);
-	}
-
-private:
-	Node<T>*	_head;
-	Node<T>*	_tail;
-	int			_size;
-};
-
-int idxCal = 1;
-int N;
+}
 
 int main()
 {
-	cin >> N;
-
-	List<int> li;
-
-	for (int i = 0; i < N; i++)
-	{
-		int input;
-		cin >> input;
-		pair<int, int> myPair = make_pair(i + 1, input);
-		// pair를 list에 넣고 싶은데 방법을 모르겠음
-		li.push_back(input, i + 1);
-	}
-
-	List<int>::iterator it = li.begin();
-
-	int num = *li.begin();
-
-	while (true)
-	{
-		// 출력
-		cout << &it << ' ';
-		
-		if (li.size() == 1) break;
-
-		// 삭제
-		num = *it;
-		it = li.erase(it);
-
-		// 이동
-		if (num >= 0)
-		{
-
-			if (it == li.end())
-				it = li.begin();
-			for (int i = 1; i < num; i++)
-			{
-				if (it == li.end())
-				{
-					it = li.begin();
-					continue;
-				}
-				it++;
-			}
-		}
-		else
-		{
-			for (int i = 0; i > num; i--)
-			{
-				if (it == li.begin())
-				{
-					it = li.end();
-					it--;
-					continue;
-				}
-				it--;
-			}
-		}
-
-	}
-
+	seq = vector<int>{ 1, 9, 2, 5, 7 };
 
 	return 0;
 }
