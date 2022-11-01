c02f2e70781644c45cfa4722520448371f7a9e1f
stopresent <mamy4938@gmail.com>
2022-11-01 16:20:33 +0900

    LIS 문제 정답

--- HasoyeonCPP/HasoyeonCPP.cpp
+++ HasoyeonCPP/HasoyeonCPP.cpp
@@ -27,16 +27,51 @@ ex) 1 2 5 7 = 길이 4
 
 */
 
+int cache[100];
 vector<int> seq;
 
-int LIS()
+int LIS(int pos)
 {
+	// 기저 사항 (예외 처리)
+	if (pos == seq.size() - 1)
+		return 1;
 
+	// 캐시 확인
+	int& ret = cache[pos];
+	if (ret != -1)
+		return ret;
+
+	// 구하기
+
+	// Seq : 1 9 2 5 7
+	// 1 9 = 2
+	// 1 2 5 7 = 4
+	// 1 5 7 = 3
+	// 1 7 = 2
+
+	// 최소 seq[pos]는 있으니 1부터 시작
+	ret = 1;
+
+	// 구하기
+	for (int next = pos + 1; next < seq.size(); next++)
+		if (seq[pos] < seq[next])
+			ret = max(ret, 1 + LIS(next));
+
+	return ret;
 }
 
 int main()
 {
+	::memset(cache, -1, sizeof(cache));
+
 	seq = vector<int>{ 1, 9, 2, 5, 7 };
 
+	int ret = 0;
+	for (int pos = 0; pos < seq.size(); pos++)
+		ret = max(ret, LIS(pos));
 	return 0;
 }
+
+// 재귀
+// 캐싱
+// 루프로 베스트 케이스 찾기
