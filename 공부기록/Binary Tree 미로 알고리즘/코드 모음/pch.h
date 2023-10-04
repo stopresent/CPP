1e1863a29dfa266b3c7bf5e1e8830428b39baa8e
stopresent <86364202+stopresent@users.noreply.github.com>
2023-10-04 11:05:15 +0900

    미로 맵 만들기 코드 모음

---
+++ 공부기록/Binary Tree 미로 알고리즘/코드 모음/pch.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "Types.h"
+#include <Windows.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <list>
+
+using namespace std;
+
+
+struct Pos
+{
+	bool operator==(Pos& other)
+	{
+		return y == other.y && x == other.x;
+	}
+
+	bool operator!=(Pos& other)
+	{
+		return !(*this == other);
+	}
+
+	Pos operator+(Pos& other)
+	{
+		Pos ret;
+		ret.y = y + other.y;
+		ret.x = x + other.x;
+		return ret;
+	}
+
+	Pos& operator+=(Pos& other)
+	{
+		y += other.y;
+		x += other.x;
+		return *this;
+	}
+
+	int y = 0;
+	int x = 0;
+};
+
+enum Dir
+{
+	DIR_UP = 0,
+	DIR_LEFT = 1,
+	DIR_DOWN = 2,
+	DIR_RIGHT = 3,
+
+	DIR_COUNT = 4
+};
